c13a84d61224cb9058cfed312e7e815f6a4a444d
Euvinni <marcosvdsc1212@gmail.com>
2023-08-10 22:19:10 -0300

    Adicionado mudanças no contexto criado

--- api-projeto/vite.config.js
+++ api-projeto/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import postcss from '../api-projeto/src/postcss.config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss
+  }
 })
